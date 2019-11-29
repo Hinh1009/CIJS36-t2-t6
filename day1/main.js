f7385b1b32f86a4e6c4cbd8cb0bca5778fd09c00
Hinh1009 <dinhhinh10091998@gmail.com>
2019-11-30 02:27:51 +0700

    day1

---
+++ day1/main.js
@@ -0,0 +1,29 @@
+// console.log("Hello world");
+// print("Hello world");
+
+// function print(content) {
+//     console.log(content)
+
+// }
+// Cau lenh dieu kien
+//boolean true/false
+// function switchCase(number) {
+//     switch (number) {
+//         case 1:
+//             {
+//                 console.log(1)
+//                 break
+//             }
+//         case 2:
+//             {
+//                 console.log(2)
+//                 break
+//             }
+//     }
+// }
+// switchCase();
+// VOng lap for
+// let numbers = [1, 2, 3]
+// for (let number of numbers) {
+//     console.log(number)
+// }
