fb37445177ada2fdd6a81cacb4ed6fe6ef6c60da
Hinh1009 <dinhhinh10091998@gmail.com>
2019-11-30 02:37:27 +0700

    day2

---
+++ New folder/main.js
@@ -0,0 +1,22 @@
+//1. Event
+// window.onload = doSomething
+// window.onclick = doSomething
+
+// function doSomething() {
+//     console.log('Doing something');
+// }
+//2. DOM
+// Window.onload = init
+
+// function init() {
+//     let forms = document.getElementsByClassName('form-header')
+//     let form = forms[0]
+
+//     form.onclick = function() {
+//         console.log("click")
+//     }
+
+//     form.style.background = 'red';
+
+//     form.innerHTML = "New content"
+// }
